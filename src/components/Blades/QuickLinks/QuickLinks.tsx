aa3bc47dea8161c3332867af06c0e596a2e1a39b
Nimisoere Lawson <lawson.nimi@yahoo.com>
2021-06-02 12:59:45 +0100

    Quick link pages

---
+++ src/components/Blades/QuickLinks/QuickLinks.tsx
@@ -0,0 +1,35 @@
+import { Link } from "gatsby"
+import React from "react"
+
+interface Props {}
+
+const QuickLinks = (props: Props) => {
+  return (
+    <nav className="flex flex-col max-w-sm">
+      <h4 className="font-medium mb-4">Quick Links</h4>
+      <Link
+        activeClassName="text-secondary"
+        className="border-b border-dotted border-geay-200 py-4"
+        to="/faq"
+      >
+        Frequently asked questions
+      </Link>
+      <Link
+        activeClassName="text-secondary"
+        className="border-b border-dotted border-geay-200 py-4"
+        to="/terms-and-conditions"
+      >
+        Terms and Conditions
+      </Link>
+      <Link
+        activeClassName="text-secondary"
+        className="border-b border-dotted border-geay-200 py-4"
+        to="/privacy-policy"
+      >
+        Privacy Policy
+      </Link>
+    </nav>
+  )
+}
+
+export default QuickLinks

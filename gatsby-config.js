a8f2cd52ace471f035d06ef4f7ccf37db619fbdc
Nimisoere Lawson <lawson.nimi@yahoo.com>
2021-06-04 14:00:59 +0100

    Remove unused plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,20 +31,5 @@ module.exports = {
         path: `${__dirname}/src`
       }
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          `gatsby-remark-relative-images`,
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false
-            }
-          }
-        ]
-      }
-    },
   ],
 }

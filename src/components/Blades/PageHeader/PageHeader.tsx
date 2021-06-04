2b68887843f8c7cd73e50c8e968822df3cc1b3dc
Nimisoere Lawson <lawson.nimi@yahoo.com>
2021-06-04 15:44:15 +0100

    Edit page Header

--- src/components/Blades/PageHeader/PageHeader.tsx
+++ src/components/Blades/PageHeader/PageHeader.tsx
@@ -14,21 +14,17 @@ const PageHeader: React.FC<Props> = ({ title, description, banner }) => {
       style={{
         backgroundImage: `url(${
           banner ||
-          "https://images.ctfassets.net/aj2hdpos2uzm/6B28FyPwALoMApWCNSS058/a901c3e47d939ab78585f186e5ee7ce0/iStock-1044275790-1.jpeg"
+          "https://images.ctfassets.net/aj2hdpos2uzm/6khB4YAxKomf1hpK0itN4f/d8ae37a91334b3efb2db63c73e53552a/stock-photo-wine-cellar-with-a-row-of-barrels-austria-706026547.jpeg"
         })`,
       }}
       className={`${styles.PageHeader} flex bg-secondary items-center`}
     >
-      <div className="h-full flex justify-center text-4xl items-center bg-opacity-50 hover:bg-opacity-75 transition-all duration-500 bg-black w-full">
+      <div className="h-full flex justify-center text-4xl items-center bg-opacity-70 transition-all duration-500 bg-black w-full">
         <Helmet title={title} defaultTitle="Tulia Wine" defer={false} />
 
         <div className="container mx-auto text-white">
           <div className="max-w-xl mx-auto text-center">
-            <h1
-              className={`font-thin ${
-                banner ? "text-secondary" : "text-white"
-              } text-5xl mb-4`}
-            >
+            <h1 className={`font-light text-secondary text-5xl mb-4`}>
               {title}
             </h1>
             <p className="text-4xl font-thin mb-4 font-serif">{description}</p>
